7aefbcde5307880e83dccd17e8179b47b6f13e30
dengmin <1622409209@qq.com>
2018-05-28 20:19:06 +0800

    first

---
+++ app/src/main/java/dengmin/cn/edu/nuc/my_weather/MainActivity.java
@@ -0,0 +1,13 @@
+package dengmin.cn.edu.nuc.my_weather;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/test/java/dengmin/cn/edu/nuc/my_weather/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package dengmin.cn.edu.nuc.my_weather;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
